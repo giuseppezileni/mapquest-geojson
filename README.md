00d36b5c5022c6b8d4ea91a171c09d54cf8e017b
Giuseppe Zileni <gzileni@icloud.com>
2015-01-01 21:56:54 +0100

    1.0.0
    First Commit

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-mapquest-geojson
-================
+## mapquest-geojson
 
-NodeJS Module to get data from OpenStreetMap API and convert to GeoJSON Data
+## install
+
+<code> npm install mapquest-geojson </code>
+
+## Config
+Update your API Key Mapquest API Developer
+
+## usage
+<code> var mapquest = require('mapquest-geojson');</code>
+
+<h2>Geocodind</h2>
+Get all geographical infomraton about street's 
+
+<code> geocode (street, city, state, format, function (data) {
+// your code
+}); </code>
+
+## Response
+<code>
+data: {
+    response: [], // response format about information <a href="http://open.mapquestapi.com/geocoding/#parameters" target="_blank"> here </a>
+    geojson: {} // gejson format <a href="http://geojson.org" target="_blank"> here </a>
+}
+</code>
+
---
+++ index.js
@@ -0,0 +1,112 @@
+var _ = require('underscore');
+var request = require("request");
+
+module.exports.version = '1.0.0';
+
+module.exports = {
+  _appkey: 'Fmjtd%7Cluurn162n0%2C7a%3Do5-9wtsuw', // Your API KEY !!!!!!
+  _host: 'http://open.mapquestapi.com',
+  _data: {
+    response: [],
+    geojson: {
+        type : "FeatureCollection",
+        features: []
+    },
+  },
+
+  _create_geojson_point: function (lat, lng, title) {
+
+    var feature = {
+        "type": "Feature",
+        "properties": {
+          "point": 'geolocation',
+          "title": title,
+          "icon": 'geolocation',
+          "color": 'green',  
+          "location": [Number(lat),
+                         Number(lng)]  
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [Number(lng),
+                         Number(lat)]
+        }
+    };
+
+    return feature;
+
+  },
+
+  geocode: function (street, city, state, format, callback) {
+    var features = new Array();
+    var self = this;
+    var q = street + ' ' + city + ' ' + state;
+    console.log('geocoding by ' + q); 
+
+    var q = {
+      location: {
+        street: street + ', ' + city + ', ' + state
+      },
+      options: {
+        thumbMaps: true,
+        maxResults: -1
+      }
+    };
+
+    var u = this._host + '/geocoding/v1/address?key=' + 
+            this._appkey + '&outFormat=json&inFormat=json&' +
+            'json=' + JSON.stringify(q);
+
+    console.log(u);
+
+    var r = { 
+      location: {
+        lat: 0,
+        lng: 0
+      },
+      data: {}
+    };
+
+    request(u, function (error, response, body) {
+
+      if (!error && response.statusCode == 200) {
+
+        var b = JSON.parse(body);
+        var i = 0;
+
+        while (b.results[i]) {
+          var rs = b.results[i];
+          if (_.size(rs.locations) > 0) {
+            var j = 0;
+            while (rs.locations[j]) {
+              var location = rs.locations[j];
+              r.location.lat = location.displayLatLng.lat;
+              r.location.lng = location.displayLatLng.lng;
+              r.data = rs;
+              self._data.response.push(r);
+              var title = location.street + ', ' +  location.postalCode;
+              var features = self._create_geojson_point(location.displayLatLng.lat, location.displayLatLng.lng, title);
+              self._data.geojson.features.push(features);
+              j++;  
+            };
+          };
+          i++;
+        }
+      } 
+
+      if (typeof callback === 'function') {
+        console.log(r);
+        if (typeof format === 'undefined') {
+          // callback all data
+          callback(self._data);
+        } else if (format === 'geojson') {
+          callback(self._data.geojson);
+        } else if (format === 'json') {
+          callback(self._data.response);
+        }
+      };
+
+    });      
+  }
+    
+};

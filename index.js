6afc20f035207a8000833fdbb603205f529560a0
Giuseppe Zileni <gzileni@icloud.com>
2015-01-15 19:49:56 +0100

    2.0.0
    Bug fixed

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var _ = require('underscore');
 var request = require("request");
 
-module.exports.version = '1.0.3';
+module.exports.version = '2.0.0';
 
 module.exports = {
   _appkey: 'Fmjtd%7Cluurn162n0%2C7a%3Do5-9wtsuw', // Your API KEY !!!!!!
   _host: 'http://open.mapquestapi.com/geocoding/v1/',
+  _host_route: 'http://open.mapquestapi.com/directions/v2/',
   _data: {
     response: [],
     geojson: {
@@ -37,7 +38,7 @@ module.exports = {
 
   },
 
-  reverse: function (lat, lng, format, callback) {
+  reverse: function (lat, lng, callback) {
     
     var self = this;
     
@@ -91,26 +92,20 @@ module.exports = {
         }
 
         if (typeof callback === 'function') {
-          console.log(r);
-          if (typeof format === 'undefined') {
-            // callback all data
-            callback(self._data);
-          } else if (format === 'geojson') {
-            callback(self._data.geojson);
-          } else if (format === 'json') {
-            callback(self._data.response);
-          }
+          callback(self._data);
         };
       }
     });
 
   },
 
-  geocode: function (street, city, state, format, callback) {
+  geocode: function (street, city, state, callback) {
     var features = new Array();
+    this._data.response = [];
+    this._data.geojson.features = [];
     var self = this;
     var q = street + ' ' + city + ' ' + state;
-    console.log('geocoding by ' + q); 
+    // console.log('geocoding by ' + q); 
 
     var q = {
       location: {
@@ -126,7 +121,7 @@ module.exports = {
             '&outFormat=json&inFormat=json&' +
             'json=' + JSON.stringify(q);
 
-    console.log(u);
+    // console.log(u);
 
     var r = { 
       location: {
@@ -164,18 +159,84 @@ module.exports = {
       } 
 
       if (typeof callback === 'function') {
-        console.log(r);
-        if (typeof format === 'undefined') {
-          // callback all data
           callback(self._data);
-        } else if (format === 'geojson') {
-          callback(self._data.geojson);
-        } else if (format === 'json') {
-          callback(self._data.response);
-        }
       };
 
     });      
+  }, 
+
+  route: function (origin, destination, city, state, callback) {
+
+    // {locations:["Via Sparano, Bari, IT","Via Massaua, Bari, IT"],options:{avoids:[],avoidTimedConditions:false,doReverseGeocode: true,shapeFormat:raw,generalize:0,routeType:bicycle,timeType:1,locale:it_IT,unit:k,enhancedNarrative:false,drivingStyle: 2,highwayEfficiency: 21.0}}
+
+    var o = origin + ', ' + city + ', ' + state;
+    var e = destination + ', ' + city + ', ' + state;
+
+    var qJson = {
+      locations:[o,e],
+      options:{ 
+        avoids: [],
+        avoidTimedConditions: false,
+        doReverseGeocode: true,
+        shapeFormat: 'raw',
+        generalize: 0,
+        routeType: 'bicycle',
+        timeType: 1,
+        locale: 'it_IT',
+        unit: 'k',
+        enhancedNarrative: false,
+        drivingStyle: 2,
+        highwayEfficiency: 21.0
+      }
+    };
+
+    var qStr = JSON.stringify(qJson);
+
+    var u = this._host_route + 'route?key=' + this._appkey +
+            '&outFormat=json&inFormat=json' +
+            '&json=' + qStr;
+    
+    this._routing(u, callback);
+
+  },
+
+  routeLatLng: function (latS, lngS, latD, lngD, callback) {
+
+    // http://www.mapquestapi.com/directions/v2/route?key=YOUR_KEY_HERE&
+    // callback=renderAdvancedNarrative&ambiguities=ignore&outFormat=json&inFormat=json&json={locations:["Via Sparano, Bari, IT","Via Massaua, Bari, IT"],options:{avoids:[],avoidTimedConditions:false,doReverseGeocode: true,shapeFormat:raw,generalize:0,routeType:bicycle,timeType:1,locale:it_IT,unit:k,enhancedNarrative:false,drivingStyle: 2,highwayEfficiency: 21.0}}
+
+    var u = this._host_route + 'route?key=' + this._appkey +
+            '&outFormat=json' +
+            '&timeType=1&enhancedNarrative=false&shapeFormat=raw&generalize=0&locale=it_IT&unit=k' +
+            '&routeType=bicycle' + 
+            '&from=' + latS + ',' + lngS + 
+            '&to=' + latD + ',' + lngD + 
+            '&drivingStyle=2&highwayEfficiency=21.0';
+    console.log(u);
+
+    this._routing(u, callback);
+
+  },
+  
+  _routing: function (url, callback) {
+    
+    var self = this;
+    var route_data;
+
+    console.log(url);
+
+    request(url, function (error, response, body) {
+
+      if (!error && response.statusCode == 200) {
+        route_data = JSON.parse(body);
+      };
+
+      if (typeof callback === 'function') {
+        callback(route_data);
+      };
+
+    });
+            
   }
     
 };
